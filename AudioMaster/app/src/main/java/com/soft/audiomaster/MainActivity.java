25a0446e992efc26b1f53ab10bb7542602d52d53
Wuang02 <markustimberg@gmail.com>
2023-03-24 19:17:54 +0700

    first commit

---
+++ AudioMaster/app/src/main/java/com/soft/audiomaster/MainActivity.java
@@ -0,0 +1,67 @@
+package com.soft.audiomaster;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViewPager();
+    }
+
+    private void initViewPager() {
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewpagerAdapter viewpagerAdapter = new ViewpagerAdapter(getSupportFragmentManager());
+        viewpagerAdapter.addFragments(new SongsFragment(), "Songs");
+        viewpagerAdapter.addFragments(new AlbumFragment(), "Albums");
+        viewPager.setAdapter(viewpagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    public static class ViewpagerAdapter extends FragmentPagerAdapter {
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+        public ViewpagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+        void addFragments(Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position){
+            return titles.get(position);
+        }
+    }
+}
